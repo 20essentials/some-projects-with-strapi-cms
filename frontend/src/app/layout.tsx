71dba8b55094d99e825a5b8d6e0d892ec25b9a15
DominicDev <s0l0estudi4r@gmail.com>
2026-02-17 16:20:46 +0100

    v0.3.6

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { inter } from '@/lib/fonts';
 
 export const metadata: Metadata = {
-  title: 'Strapi Practice',
+  title: 'Some projects and parts of code using Strapi',
   description: 'Practicing with strapi',
   icons: {
     icon: '/assets/favicon.png'
